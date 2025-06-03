70a6f5863948706991f32baa3ac27017f8b3266e
Paulo-Sergio-Oliveira <paulosergiooliveira113@gmai.com>
2025-06-03 16:34:45 -0300

    Versionamento e Definição de Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\InvoiceController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//API
+Route::middleware('auth_sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix'=> 'api', 'namespace' => 'App\Http\Controllers\API'], function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
 });
